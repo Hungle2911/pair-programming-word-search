68e3c282e8e15ddc947fe32951484b421f1839d7
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-02-25 15:21:09 -0500

    Defined l and credit comment

--- wordsearch.js
+++ wordsearch.js
@@ -1,39 +1,40 @@
-const transpose = function (matrix) {
-    // Replace this code with your solution
-    // create new matrix, results, with empty arrays = length of matrix[0]
-    let result = [];
+// Credit: Hung Le (@Hungle2911), Cynthia Lam (@cynthia-lam), Lubi Islam(@lubi25)
+const transpose = function(matrix) {
+  // Replace this code with your solution
+  // create new matrix, results, with empty arrays = length of matrix[0]
+  let result = [];
     
-    const numRows = matrix.length;
-    const numCols = matrix[0].length;
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
   
-    // fill these with num from matrix:
-    for (let col = 0; col < numCols; col++) {
-      const newRow = [];
+  // fill these with num from matrix:
+  for (let col = 0; col < numCols; col++) {
+    const newRow = [];
   
-      for (let row = 0; row < numRows; row++) {
-          newRow.push(matrix[row][col]);
-      }
-      result.push(newRow);
+    for (let row = 0; row < numRows; row++) {
+      newRow.push(matrix[row][col]);
     }
+    result.push(newRow);
+  }
   
-    return result;
-  };
+  return result;
+};
 
-const wordSearch = (letters, word) => { 
-    //Horizontal
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true;
-    }
+const wordSearch = (letters, word) => {
+  //Horizontal
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-    //Vertical
-    const lettersTransposed = transpose(letters);
-    const verticalJoin = lettersTransposed.map(ls => ls.join(''))
-    for (l of verticalJoin) {
-        if (l.includes(word)) return true;
-    }
+  //Vertical
+  const lettersTransposed = transpose(letters);
+  const verticalJoin = lettersTransposed.map(ls => ls.join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
 
-    return false;
-}
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
